b7a038a72ee78e987ea40f610711c3f84bf6b9ae
dx1dev <122834463+dx1dev@users.noreply.github.com>
2026-09-14 13:21:41 +0000

    Delete all files

--- tab-google.js
+++
@@ -1,8 +0,0 @@
-// Sugestões de pesquisa rápida
-function quickSearch(query) {
-  var searchInput = document.querySelector('#tab-google input[type="text"]');
-  if (searchInput) {
-    searchInput.value = query;
-    if (searchInput.form) searchInput.form.submit();
-  }
-}
